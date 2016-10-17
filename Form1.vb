9b6fc7fe1ad485c9c6b97add5e378c1418318962
cmczerny <cmczerny@oakland.edu>
2016-10-17 17:42:31 -0400

    Delete Form1.vb

--- Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Label1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnReturn.Click
-
-    End Sub
-End Class
