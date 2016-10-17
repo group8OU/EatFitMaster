3b19ef7b1e7ebdc15fbebbc8a8481c01c1c7e3ce
Chad Czerny <cmczerny@oakland.edu>
2016-10-17 17:55:33 -0400

    Added loading sequence for forms

--- EatFitMaster - Ian/WindowsApplication1/Login.vb
+++ EatFitMaster - Ian/WindowsApplication1/Login.vb
@@ -4,10 +4,16 @@
     End Sub
 
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnReturn.Click
-
+        frmWelcome.Show()
+        Me.Hide()
     End Sub
 
     Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
+        window_basicinfo.Show()
+        Me.Hide()
     End Sub
 End Class
