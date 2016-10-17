23a4b486bd76a07ac686cd84c7a51a56d72e917e
cmczerny <cmczerny@oakland.edu>
2016-10-17 17:10:58 -0400

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnReturn.Click
+
+    End Sub
+End Class
